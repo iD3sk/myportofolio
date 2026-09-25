228c0d2a3d9543b9ee6e8677c66e315aa2fec0c9
iD3sk <fiqhideski@gmail.com>
2026-09-26 01:00:20 +0700

    Tutorial 4: bagian 1

--- main/urls.py
+++ main/urls.py
@@ -6,11 +6,16 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.show_main, name="show_main"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+
     path("experiences/add/", views.create_experience, name="create_experience"),
     path("experiences/", views.show_experiences, name="show_experiences"),
     path("experiences/<uuid:experience_id>/edit/", views.update_experience,name="update_experience",),
     path("experiences/<uuid:experience_id>/delete/", views.delete_experience, name="delete_experience"),
     path("api/experience/", views.get_experience_json, name="get_experience_json"),
+
     path("about/", views.show_about, name="show_about"),
     path("about/achievements/add/", views.create_achievement, name="create_achievement"),
     path("about/achievements/<uuid:achievement_id>/edit/", views.update_achievement, name="update_achievement"),
--- main/views.py
+++ main/views.py
@@ -3,8 +3,46 @@ from django.shortcuts import get_object_or_404, redirect, render
 from main.models import Experience, Educations, Achievements
 from main.forms import ExperienceForm, AchievementForm
 from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
 from django.core import serializers
 from django.http import HttpResponse
+
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Akun berhasil dibuat. Silakan login.")
+        return redirect("main:login")
+
+    context = {
+        "name": "Fiqhi",
+        "form": form,
+    }
+    return render(request, "register.html", context)
+
+
+def login_user(request):
+    form = AuthenticationForm(request, data=request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        login(request, form.get_user())
+        return redirect("main:show_main")
+
+    context = {
+        "name": "Burhan",
+        "form": form,
+    }
+    return render(request, "login.html", context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("main:show_main")
+
 
 
 def show_main(request):
